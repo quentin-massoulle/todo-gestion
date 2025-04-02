a7508f1f31e0e1f6998bf4923ece357edb6b78f9
quentin-massoulle <quentin.massoulledu80@gmail.com>
2025-04-02 21:17:27 +0200

    mise en place de fichier de langue

---
+++ resources/lang/en/message.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'welcome' => 'welcome in the webSite',
+    'goodbye' => 'Goodbaey',
+];
---
+++ resources/lang/fr/message.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'welcome' => 'Bienvenue sur notre site!',
+    'goodbye' => 'Au revoir!',
+];
