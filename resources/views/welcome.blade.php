dbc83ba7fa6cce26baf701a12eef41008eb63512
quentin-massoulle <quentin.massoulledu80@gmail.com>
2025-04-09 23:19:50 +0200

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,25 +18,25 @@
   <!-- Tâches Section -->
   <section id="tasks" class="py-16 text-center bg-[#4F96FF] bg-opacity-40">
     <h2 class="text-3xl font-semibold mb-4">
-        <i class="fas fa-list-ul mr-2"></i>Gérez vos Tâches Facilement
+        Gérez vos Tâches Facilement
     </h2>    
     <p>Avec TodoGestion, vous pouvez facilement organiser vos tâches quotidiennes. Que ce soit pour le travail, les études ou la maison, ajoutez, suivez et accomplissez vos tâches en toute simplicité.</p>
     <div class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
       <div class="card bg-[#61D0A7]">
         <h3 class="text-xl font-semibold mb-4">
-            <i class="fas fa-list mr-2"></i>Organisez vos Tâches
+            <i class="fas fa-list mr-2"></i>
         </h3>
         <p >Créez des listes de tâches pour chaque aspect de votre vie. Priorisez, ajoutez des descriptions et des échéances pour ne rien oublier.</p>
       </div>
       <div class="card bg-[#61D0A7]">
         <h3 class="text-xl font-semibold mb-4">
-            <i class="fas fa-chart-line mr-2"></i>Suivi des Progrès
+            <i class="fas fa-chart-line mr-2"></i>
         </h3>        
         <p >Suivez l'avancement de vos tâches, marquez-les comme terminées et célébrez chaque petite victoire !</p>
       </div>
       <div class="card bg-[#61D0A7]">
         <h3 class="text-xl font-semibold mb-4">
-            <i class="fas fa-bell mr-2"></i>Rappels Automatiques
+            <i class="fas fa-bell mr-2"></i>
         </h3>        
         <p >Recevez des notifications pour ne jamais oublier une tâche importante et restez productif tout au long de la journée.</p>
       </div>
@@ -46,19 +46,19 @@
   <!-- Budget Section -->
   <section id="budget" class="bg-[#F3F4F6] bg-opacity-40 py-16 text-center">
     <h2 class="text-3xl font-semibold mb-4">
-        <i class="fas fa-wallet mr-2"></i>Suivi de Votre Budget
+        Suivi de Votre Budget
     </h2>
     <p>Gérer votre budget n'a jamais été aussi facile. TodoGestion vous aide à suivre vos revenus, vos dépenses et à garder un œil sur vos économies.</p>
     <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
       <div class="card bg-[#61D0A7]">
         <h3 class="text-xl font-semibold mb-4">
-            <i class="fas fa-money-bill-wave mr-2"></i>Suivi des Revenus
+            <i class="fas fa-money-bill-wave mr-2"></i>
         </h3>        
         <p>Ajoutez vos sources de revenus et surveillez vos entrées d'argent facilement pour mieux gérer votre budget mensuel.</p>
       </div>
       <div class="card bg-[#61D0A7]">
         <h3 class="text-xl font-semibold mb-4">
-            <i class="fas fa-credit-card mr-2"></i>Suivi des Dépenses
+            <i class="fas fa-credit-card mr-2"></i>
         </h3>        
         <p>Ajoutez vos dépenses pour suivre où va votre argent. Gardez un œil sur votre budget pour éviter les mauvaises surprises.</p>
       </div>
@@ -68,26 +68,27 @@
 <!-- Stats Section -->
 <section id="stats" class="py-16 text-center bg-[#4F96FF] bg-opacity-40 ">
     <h2 class="text-3xl font-semibold mb-4">
-        <i class="fas fa-chart-line mr-2"></i>Statistiques
+        Statistiques
     </h2>    
     <p>Gardez un œil sur vos progrès grâce aux statistiques en temps réel. Suivez l'achèvement de vos tâches, l'utilisation de votre budget et vos économies pour une gestion plus efficace de votre quotidien.</p>
     <div class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
       <div class="card bg-[#61D0A7]">
         <h3 class="text-xl font-semibold mb-4">
-            <i class="fas fa-check-circle mr-2"></i>Tâches Terminées
+            <i class="fas fa-check-circle mr-2"></i>
         </h3>        
         <p>Gardez un historique de vos tâches terminées pour voir vos progrès au fil du temps.</p>
       </div>
       <div class="card bg-[#61D0A7]">
         <h3 class="text-xl font-semibold mb-4">
-            <i class="fas fa-wallet mr-2"></i>Budget Utilisé
+            <i class="fas fa-wallet mr-2"></i>
         </h3>        
         <p>Suivez l'évolution de votre budget et la proportion de vos dépenses par rapport à vos revenus.</p>
       </div>
       <div class="card bg-[#61D0A7]">
         <h3 class="text-xl font-semibold mb-4">
-            <i class="fas fa-piggy-bank mr-2"></i>Économies Réalisées
-        </h3>        
+            <i class="fas fa-piggy-bank mr-2"></i>
+        </h3>
+        
         <p>Consultez vos économies réalisées pour mieux planifier vos objectifs financiers.</p>
       </div>
     </div>
