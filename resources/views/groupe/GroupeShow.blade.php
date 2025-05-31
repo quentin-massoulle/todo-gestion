08d320b6ec3cdcf208e0d6e9958b337800110754
quentin-massoulle <quentin.massoulledu80@gmail.com>
2025-05-31 12:22:59 +0200

    twip pages des groupe

--- resources/views/groupe/GroupeShow.blade.php
+++ resources/views/groupe/GroupeShow.blade.php
@@ -9,13 +9,28 @@
 @endsection
 
 @section('style')
-  <link rel="stylesheet" href="{{ asset('css/dashboardUser.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/showGroupe.css') }}">
 @endsection
 
 @section('content')
 <section class="hero">
     <div class="mx-auto">
         <h2 class="text-4xl font-semibold mb-4">gestion du groupe</h2>
+        <div class="containeur">
+            <div class="containeurIner containeurTask">
+
+          </div>
+            <div class="containeurIner containeurDiscution">
+                <div class="chat">
+                  <div class="message-box">
+                    <textarea style="resize: none" name="" id="" ></textarea>
+                  </div>
+                  <div class="message-channel">
+
+                  </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
